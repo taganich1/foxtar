bba49087a3f78a6398a56f174ecd7e36fd68fec3
taganich <anton.12345.123@gmail.com>
2020-10-12 20:12:07 +0300

    added all-items

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix:"$",
+        skin:"round"
+    });
+});
+
--- gulpfile.js
+++ gulpfile.js
@@ -41,7 +41,8 @@ gulp.task('js', function(){
     return gulp.src(
         ['node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
         'node_modules/mixitup/dist/mixitup.js',
-        'node_modules/slick-carousel/slick/slick.js'
+        'node_modules/slick-carousel/slick/slick.js',
+        'node_modules/ion-rangeslider/js/ion.rangeSlider.js'
         ])
         .pipe(concat('libs.min.js'))
         .pipe(uglifyjs())
